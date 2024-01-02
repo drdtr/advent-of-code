7310af7db6502348141fca4747ea6e3590b988a2
David Trachtenherz <d.trachtenherz+git@gmail.com>
2024-01-02 14:37:17 +0100

    add solution for year 2017, day 03, part 2, puzzle "Spiral Memory"

--- src/main/kotlin/advent_of_code_2017/day03/SpiralMatrix.kt
+++ src/main/kotlin/advent_of_code_2017/day03/SpiralMatrix.kt
@@ -1,16 +1,18 @@
 package advent_of_code_2017.day03
 
 import Util.readInputLines
+import advent_of_code_2017.day03.SpiralMatrix.SpiralCellCoordinatesIterator.Direction.*
 import kotlin.math.abs
 import kotlin.math.sqrt
 
 /**
  * [Spiral Memory](https://adventofcode.com/2017/day/3)
  *
- * Given a spiral matrix, starting in the center, find the Manhattan distance to a given cell `n`.
+ * You're given a spiral matrix, that starts in the center with `1` and is filled with positive integers.
  *
  * ### Part 1
  *
+ * Find the Manhattan distance to a given cell `n`. *
  * The matrix cells are filled with positive integers, starting at 1:
  * ```
  * 37  36  35  34  33  32  31
@@ -26,6 +28,19 @@ import kotlin.math.sqrt
  *   - `12` - distance is 3
  *   - `23` - distance is 2
  *   - `1024` - distance is 31
+ *
+ *
+ * ### Part 2
+ *
+ * Find the first cell value larger than a given `n`.
+ * The matrix cells are filled with the sums of all 8 neighboring cells, starting at 1:
+ * ```
+ * 147  142  133  122   59
+ * 304    5    4    2   57
+ * 330   10    1    1   54
+ * 351   11   23   25   26
+ * 362  747  806--->   ...
+ * ```
  *
  */
 class SpiralMatrix {
@@ -80,6 +95,83 @@ class SpiralMatrix {
         if (r > 0) error("Wrong spiral - this should never happen.")
         return Point(x, y)
     }
+
+    class SpiralCellCoordinatesIterator : Iterator<Point> {
+        private enum class Direction {
+            Up, Down, Left, Right
+        }
+
+        private var x = 0
+        private var y = 0
+        private var spiralLevel = 1
+        private var direction = Right
+
+        override fun hasNext() = true
+
+        override fun next(): Point {
+            val currPoint = Point(x, y)
+            when (direction) {
+                Right -> {
+                    if (x < spiralLevel) {
+                        x++
+                    } else {
+                        direction = Up
+                        y++
+                    }
+                }
+
+                Up    -> {
+                    if (y < spiralLevel) {
+                        y++
+                    } else {
+                        direction = Left
+                        x--
+                    }
+                }
+
+                Left  -> {
+                    if (-x < spiralLevel) {
+                        x--
+                    } else {
+                        direction = Down
+                        y--
+                    }
+                }
+
+                Down  -> {
+                    if (-y < spiralLevel) {
+                        y--
+                    } else {
+                        direction = Right
+                        spiralLevel++
+                        x++
+                    }
+                }
+            }
+            return currPoint
+        }
+
+    }
+
+    fun firstSumMatrixValueGreaterThan(n: Int): Int {
+        val cells = hashMapOf<Point, Int>()
+        val coordItr = SpiralCellCoordinatesIterator()
+        cells[coordItr.next()] = 1 // initial cell (0, 0)
+
+        var cellValue = 1
+        while (cellValue <= n) {
+            val p = coordItr.next()
+            var sum = 0
+            for (dx in -1..1) for (dy in -1..1) {
+                if (dx == 0 && dy == 0) continue
+                sum += cells[Point(p.x + dx, p.y + dy)] ?: 0
+            }
+            cellValue = sum
+            cells[p] = cellValue
+        }
+
+        return cellValue
+    }
 }
 
 private fun readInput(inputFileName: String): Int =
@@ -88,10 +180,12 @@ private fun readInput(inputFileName: String): Int =
 private fun printResult(inputFileName: String) {
     val n = readInput(inputFileName)
     val solver = SpiralMatrix()
-
     // part 1
     val res1 = solver.calcManhattanDistance(n)
     println("Manhattan distance to cell $n: $res1")
+    // part 2
+    val res2 = solver.firstSumMatrixValueGreaterThan(n)
+    println("First sum matrix value greater than $n: $res2")
 }
 
 fun main() {
--- src/test/kotlin/advent_of_code_2017/day03/SpiralMatrixTest.kt
+++ src/test/kotlin/advent_of_code_2017/day03/SpiralMatrixTest.kt
@@ -16,6 +16,13 @@ internal class SpiralMatrixTest {
         assertEquals(expected, calcCellCoordinates(n))
     }
 
+    @ParameterizedTest
+    @ArgumentsSource(ArgumentsProviderSpiralMatrixCellCoordinates::class)
+    fun `test SpiralCellCoordinatesIterator`(expected: Point, n: Int) = with(SpiralCellCoordinatesIterator()) {
+        repeat(n - 1) { next() }
+        assertEquals(expected, next())
+    }
+
     private class ArgumentsProviderSpiralMatrixCellCoordinates : ArgumentsProvider {
         override fun provideArguments(context: ExtensionContext?) = Stream.of(
             Arguments.of(Point(0, 0), 1),
@@ -45,6 +52,7 @@ internal class SpiralMatrixTest {
             Arguments.of(Point(2, -2), 25),
         )
     }
+
     @ParameterizedTest
     @ArgumentsSource(ArgumentsProviderSpiralMatrix::class)
     fun `test calcManhattanDistance`(expected: Int, n: Int) = with(SpiralMatrix()) {
@@ -57,6 +65,68 @@ internal class SpiralMatrixTest {
             Arguments.of(3, 12),
             Arguments.of(2, 23),
             Arguments.of(31, 1024),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(ArgumentsProviderFirstSumMatrixValueGreaterThanGiven::class)
+    fun `test firstSumMatrixValueGreaterThan`(expected: Int, n: Int) = with(SpiralMatrix()) {
+        assertEquals(expected, firstSumMatrixValueGreaterThan(n))
+    }
+
+    private class ArgumentsProviderFirstSumMatrixValueGreaterThanGiven : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?) = Stream.of(
+            Arguments.of(1, 0),
+            Arguments.of(2, 1),
+            Arguments.of(4, 2),
+            Arguments.of(4, 3),
+            Arguments.of(5, 4),
+            Arguments.of(10, 5),
+            Arguments.of(10, 6),
+            Arguments.of(10, 9),
+            Arguments.of(11, 10),
+            Arguments.of(23, 11),
+            Arguments.of(23, 12),
+            Arguments.of(23, 22),
+            Arguments.of(25, 23),
+            Arguments.of(25, 24),
+            Arguments.of(26, 25),
+            Arguments.of(54, 26),
+            Arguments.of(54, 53),
+            Arguments.of(57, 54),
+            Arguments.of(57, 56),
+            Arguments.of(59, 57),
+            Arguments.of(59, 58),
+            Arguments.of(122, 59),
+            Arguments.of(122, 60),
+            Arguments.of(122, 121),
+            Arguments.of(133, 122),
+            Arguments.of(133, 123),
+            Arguments.of(133, 132),
+            Arguments.of(142, 133),
+            Arguments.of(142, 134),
+            Arguments.of(142, 141),
+            Arguments.of(147, 142),
+            Arguments.of(147, 143),
+            Arguments.of(147, 146),
+            Arguments.of(304, 147),
+            Arguments.of(304, 148),
+            Arguments.of(304, 303),
+            Arguments.of(330, 304),
+            Arguments.of(330, 305),
+            Arguments.of(330, 329),
+            Arguments.of(351, 330),
+            Arguments.of(351, 331),
+            Arguments.of(351, 350),
+            Arguments.of(362, 351),
+            Arguments.of(362, 352),
+            Arguments.of(362, 361),
+            Arguments.of(747, 362),
+            Arguments.of(747, 363),
+            Arguments.of(747, 746),
+            Arguments.of(806, 747),
+            Arguments.of(806, 748),
+            Arguments.of(806, 805),
         )
     }
 }
